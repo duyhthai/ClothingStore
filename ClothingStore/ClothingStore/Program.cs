67644572b08c3bded783d15d1da3307021e34523
Duy <duyhthai@gmail.com>
2019-01-04 22:27:42 +0700

    refactor

--- ClothingStore/ClothingStore/Program.cs
+++ ClothingStore/ClothingStore/Program.cs
@@ -2,10 +2,7 @@
 using ClothingStore.Data;
 using ClothingStore.Data.Models;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ClothingStore
 {
